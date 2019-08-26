73407b43709f0c3698fba445380cc64b747514f7
124ztzy <1247919279@qq.com>
2019-08-26 17:02:12 +0800

    上传编译好的dll

--- ReadMe.sql
+++ ReadMe.sql
@@ -14,7 +14,28 @@ select *
 from dbo.SequenceDateTime('2019-01-01 00:00:00', '2019-01-02 00:00:00', '01:00:00')
 
 
---2. 下载提取采集
+
+--2. 字符串聚合拼接
+select dbo.TextAggregate(f1, ',')
+from (
+	select 1 as f1
+	union all 
+	select 2
+) as t
+--可用for xml path代替
+select stuff((
+	select ',' + cast(f1 as varchar)
+	from (
+		select 1 as f1
+		union all 
+		select 2
+	) as t
+	for xml path('')
+), 1, 1, '')
+
+
+
+--3. 下载提取采集
 --采集天天基金净值，下载后用正则表达式匹配提取值再行转列
 --drop table #jz
 select matchNumber, '000001' as fundCode, '华夏成长混合' as fundName, [1] as tradeDate, [3] as jz, [5] as ljjz
@@ -40,24 +61,24 @@ select dbo.DownloadFileCache(
 	'12:00:00')
 
 
-
---读取Excel
+--读取Excel，可避免OLEDB莫名其妙地报错
+--需要添加程序集ICSharpCode.SharpZipLib.dll、ExcelDataReader.dll
+select [0] as tradeDate, [1] as indexCode, [2] as indexName, [4] as stockCode, [5] as stockName
+from dbo.ExcelRead('\\192.168.1.34\database\中证指数\000926cons2.xls') as t1
+pivot (max(cellValue) for columnNumber in ([0],[1],[2],[3],[4],[5],[6],[7])) as t2
+where [0] != '日期Date'
+--可用OLEDB驱动代替
 select * 
 from openrowset(
 	'Microsoft.ACE.OLEDB.12.0',
 	'Excel 8.0;hdr=no;Database=\\192.168.1.34\database\中证指数\000926cons.xls',
 	'select * from [2019-08-20$]'
 )
---可用ExcelRead代替，避免OLEDB莫名其妙地报错
---需要添加程序集ICSharpCode.SharpZipLib.dll、ExcelDataReader.dll
-select [0] as tradeDate, [1] as indexCode, [2] as indexName, [4] as stockCode, [5] as stockName
-from dbo.ExcelRead('\\192.168.1.34\database\中证指数\000926cons2.xls') as t1
-pivot (max(cellValue) for columnNumber in ([0],[1],[2],[3],[4],[5],[6],[7])) as t2
-where [0] != '日期Date'
+
 
 
 									
---3. 全局变量保存赋值
+--4. 全局变量保存赋值
 --计算每周一定投1000收益率（不考虑分红），利用全局变量进行累加运算
 select dbo.VariableAssign('number', 0)
 select dbo.VariableAssign('cost', 0.0)
@@ -89,7 +110,7 @@ order by tradeDate
 
 
 									      
---4. 调用C#反射函数
+--5. 调用C#反射函数
 --执行反射方法，格式化字符串
 exec dbo.ExecuteReflection 
 	'System.String', 
@@ -97,7 +118,7 @@ exec dbo.ExecuteReflection
 	'Format', 
 	'select ''{0:D4}'', 1 union all select ''{0:D4}'', 2',
 	0
---可用TextFormat代替
+--可用TextFormat函数代替
 select dbo.TextFormat(f1, f2)
 from (
 	select '{0:D4}' as f1, 1 as f2
@@ -121,7 +142,7 @@ exec dbo.ExecuteReflection
 	'GetFiles', 
 	null,
 	0
---可用FileTree读取文件树代替
+--可用FileTree文件树表函数代替
 select *
 from dbo.FileTree('\\192.168.1.34\database\中证指数\', 0, null)
 union all
